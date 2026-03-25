374e251532cc0c0eb60a421fa43235180c90b825
Slumfie <lucasdebacker2006@gmail.com>
2026-03-25 19:59:14 +0100

    de usernamen worden getoond!

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,61 @@
-import { useState } from 'react'
 import './App.css'
+import {useEffect, useState} from "react";
 
-function getUserComponents() {
-  let users = {};
+async function getUserComponents() {
+  let links = [];
   try {
-    users = fetchUsers();
+    links = await fetchUsers();
   } catch (error) {
-    console.log(error);
+    console.log(error + " in getUserComponents");
+  }
+  const users = [];
+  for (let link of links) {
+    const user = await fetchJSONfromUser(link);
+    users.push(user);
   }
   return (<div>
     {
-      users.map(user => makeUserComponent(fetchJSONfromUser(user)))
+      users.map((user) => makeUserComponent(user))
     }
-  </div>)
+  </div>);
 }
 
-function fetchJSONfromUser(user) {
-    return fetch('https://groep40.webdev.stud.atlantis.ugent.be/user-7/' + user.id).then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('API call for user details failed with status ' + response.status);
-      }
-    });
+async function fetchJSONfromUser(link) {
+  return fetch(link).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('API call for user details failed with status ' + response.status);
+    }
+  });
 }
 
 function makeUserComponent(user) {
   return <div>
-    <h3>Naam: {user.name}</h3>
+    <h3>Naam: {user["name"]}</h3>
   </div>
 }
 
-function fetchUsers() {
-  return fetch('https://groep40.webdev.stud.atlantis.ugent.be/user-7').then(response => {
+async function fetchUsers() {
+  let result = await fetch('https://groep40.webdev.stud.atlantis.ugent.be/user-7').then(response => {
     if (response.ok) {
-        return response.json();
+      return response;
     } else {
       throw new Error('API call for users failed with status ' + response.status);
     }
-  }).users
+  });
+  return (await result.json())["users"];
 }
 
 function App() {
-  return (<>
-    {
-      getUserComponents()
-    }
-  </>)
+  const [users, setUsers] = useState(<> </>);
+  useEffect(() => {
+    getUserComponents().then(users => setUsers(users));
+  }, [setUsers]);
+
+  return (
+      users
+  )
 }
 
 export default App
