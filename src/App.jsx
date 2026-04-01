17db28e405a1bb12b9d2805e465ec5102e3a9437
Slumfie <lucasdebacker2006@gmail.com>
2026-04-01 14:45:43 +0200

    links zijn dynamisch nu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import './App.css'
 import {useEffect, useState} from "react";
 
+async function fetchIndex() {
+  let result = await fetch('https://groep40.webdev.stud.atlantis.ugent.be/').then(response => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error('API call for users failed with status ' + response.status);
+    }
+  });
+  return (await result.json());
+}
+
 async function getUserComponents() {
   let links = [];
   try {
@@ -37,7 +48,7 @@ function makeUserComponent(user) {
 }
 
 async function fetchUsers() {
-  let result = await fetch('https://groep40.webdev.stud.atlantis.ugent.be/user-7').then(response => {
+  let result = await fetch((await fetchIndex())["users"]).then(response => {
     if (response.ok) {
       return response;
     } else {
