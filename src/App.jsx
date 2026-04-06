03020ea5ec8de8465b7cf4d1764794b6c3061702
Slumfie <lucasdebacker2006@gmail.com>
2026-04-06 16:46:52 +0200

    homepage toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Gebruikers from "./components/pages/Gebruikers.jsx";
 import Gebruiker from "./components/entities/Gebruiker.jsx";
+import Home from "./components/pages/Home.jsx";
 
 function App() {
   return (<BrowserRouter>
         <Routes>
-            <Route path="/home" element={<Gebruikers/>}></Route>
+            <Route path="/home" element={<Home/>}></Route>
             <Route path="/users" element={<Gebruikers/>}></Route>
             <Route path="/users/:url" element={<Gebruiker/>}></Route>
             <Route path="/*" element={<Navigate to={'/home'} />}></Route>
